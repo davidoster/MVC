1706aa02718942d0c6ba9e775278d23d402f3c50
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-06-28 22:26:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_MVC
+ sailsJS MVC apps
---
+++ exampleapp2/api/controllers/email-controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+  exits: {
+    success: {
+      viewTemplatePath: 'myemail'
+    }
+  },
+
+  fn: async function() {
+    return {};
+  }
+};
