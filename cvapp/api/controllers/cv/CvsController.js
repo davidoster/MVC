edd846628c82a07f86623a4ffe0eb25049cdfe16
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-06-29 20:06:16 +0300

    cvapp

---
+++ cvapp/api/controllers/cv/CvsController.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Cvs',
+
+
+  description: 'Cvs cv.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
