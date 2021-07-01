97547191bc84f573b20b57435a883dc169ad0c08
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-07-01 22:29:09 +0300

    update deleted

--- cvapp/api/controllers/products/delete.js
+++ cvapp/api/controllers/products/delete.js
@@ -4,14 +4,27 @@ module.exports = {
 
     },
     exits: {
-        success: {
-            // viewTemplatePath: 'pages/products/list'
+        error: {
+            viewTemplatePath:  'pages/products/notfound'
         },
+        // success: {
+        //     viewTemplatePath: 'pages/products/notfound'
+        // },
     },
     fn: async function(inputs) {
-        await Product.destroy(inputs.id)
-        this.res.redirect('/products') // GET localhost:1337/products
-        return {}
+        if(await Product.findOne(inputs.id)) {
+            await Product.destroy(inputs.id)
+            this.res.redirect('/products') // GET localhost:1337/products
+            // return { id: '' }
+        } else {
+            console.log('Not deleted')
+            // return {id: inputs.id}
+            throw 404
+            // throw 404 && return {id: inputs.id}
+            // throw return {error: 404, id: inputs.id}
+            // return throw {error: 404, id: inputs.id}
+            
+        }
     },
 
 }
