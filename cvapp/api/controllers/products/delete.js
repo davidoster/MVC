e7500537385eba98eb490f5aa697b551805c08b6
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-07-02 18:23:42 +0300

    corrected delete

--- cvapp/api/controllers/products/delete.js
+++ cvapp/api/controllers/products/delete.js
@@ -4,26 +4,22 @@ module.exports = {
 
     },
     exits: {
-        error: {
-            viewTemplatePath:  'pages/products/notfound'
-        },
+        // error: {
+        //     viewTemplatePath:  'pages/products/notfound'
+        // },
         // success: {
         //     viewTemplatePath: 'pages/products/notfound'
         // },
     },
-    fn: async function(inputs) {
+    fn: async function(inputs,exits) {
         if(await Product.findOne(inputs.id)) {
             await Product.destroy(inputs.id)
             this.res.redirect('/products') // GET localhost:1337/products
-            // return { id: '' }
         } else {
             console.log('Not deleted')
-            // return {id: inputs.id}
-            throw 404
-            // throw 404 && return {id: inputs.id}
-            // throw return {error: 404, id: inputs.id}
-            // return throw {error: 404, id: inputs.id}
-            
+            this.res.status(404)
+            result = `Product has not been found with id: ${inputs.id}`
+            return this.res.view('pages/products/notfound', {result})
         }
     },
 
